acff25a6fa19ed7fc58ac35075aa4a7368817264
jonathanCocuy <jonathancp145@gmail.com>
2023-02-28 17:08:26 -0500

    Commit first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MiniCalculator';
+export class AppComponent 
+{
+  title = 'Mini-Calculator';
+
+  number1:number = 0;
+  number2:number= 0;
+  result:number = 0;
+
+  AddNumber():void{
+    this.result = this.number1 + this.number2;
+  }
+
+  SubtrackNumber():void{
+    this.result = this.number1 - this.number2;
+  }
+  
+  MultiplyNumber():void{
+    this.result = this.number1 * this.number2;
+  }
+
+  DivideNumber():void{
+    this.result = this.number1 / this.number2;
+  }
+
+  ResultP():void{
+    this.result;
+  }
 }
